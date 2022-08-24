6125fe83cd41d4ef32e0e7b753231b36c9698589
Matheus Fabio Raimundo Da Silva <fabiomatheus488@gmail.com>
2022-08-23 21:24:11 -0300

    chore: adding studies about string on js

---
+++ string.js
@@ -0,0 +1,26 @@
+//      String comun
+// let umaString = "Um texto";
+//     String com uma scape
+// let umaString = 'Um "texto"';
+//  \t => cria um tab na string
+//let umaString = 'Um "\texto"';
+
+// let umaString = "Um Texte";
+// usando templade string
+// console.log(`${umaString} no javascript`);
+
+// let umaString = "Um Texte";
+// expressoes regulares
+// console.log(umaString.match(/[a-z]/g));
+
+// let umaString = "O rato roeu a roupa do rei de roma.";
+// // substitui todos os caracteres "r" por "#"
+// console.log(umaString.replace(/r/g, "#"));
+
+// let umaString = "O rato roeu a roupa do rei de roma.";
+// // busca string por indece parecido com substring so que com pouco codigo
+// console.log(umaString.slice(-5, -1));
+// console.log(umaString.slice(-5));
+
+let umaString = "O rato roeu a roupa do rei de roma.";
+console.log(umaString.split(" "));
