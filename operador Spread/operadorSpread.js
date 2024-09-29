9cab595c981e75be53e10df83ca04a875a0d4131
Matheus Fábio <matheusfabiors@gmail.com>
2024-09-28 21:11:19 -0300

    Resolve the spread operator challange

--- operador Spread/operadorSpread.js
+++ operador Spread/operadorSpread.js
@@ -1,18 +1,18 @@
-// 1. **Desafio 1:**
+✅// 1. **Desafio 1:**
 //    Crie uma função que receba dois arrays como parâmetros. Usando o operador spread, retorne um novo array que contenha todos os elementos dos dois arrays combinados, sem modificar os arrays originais.
 
-function spreadArray(array1, array2) {
-  console.warn("Spread Array");
-  const allArray = [...array1, ...array2];
+// function spreadArray(array1, array2) {
+//   console.warn("Spread Array");
+//   const allArray = [...array1, ...array2];
 
-  allArray.forEach((el) => {
-    console.warn(el);
-  });
-}
+//   allArray.forEach((el) => {
+//     console.warn(el);
+//   });
+// }
 
-const Array1 = [1, 2, 3, 4];
-const Array2 = [5, 6, 7, 8];
-spreadArray(Array1, Array2);
+// const Array1 = [1, 2, 3, 4];
+// const Array2 = [5, 6, 7, 8];
+// spreadArray(Array1, Array2);
 
 //Two forms
 // function spreadArray(array1, array2) {
@@ -31,5 +31,19 @@ spreadArray(Array1, Array2);
 // const result = spreadArray(Array1, Array2);
 // console.warn(result);
 
-// 2. **Desafio 2:**
+✅// 2. **Desafio 2:**
 //    Escreva uma função que receba um objeto e um conjunto de propriedades adicionais. Usando o operador spread, retorne um novo objeto que combine as propriedades do objeto original com as propriedades adicionais passadas para a função.
+
+// const user = { name: "John", email: "john@example.com" };
+
+// function receiveObject(param) {
+//   return { param, ...user };
+// }
+
+// //ACTUALY USER
+// console.log(user);
+
+// //NOW USER
+// const newUser = { ...user, age: 45, gener: "male" };
+
+// console.log(receiveObject(newUser));
