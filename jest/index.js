8b400ab1abe1e499615653538c850625dbbdcb24
Matheus Fábio <matheusfabiors@gmail.com>
2025-03-20 12:17:33 -0300

    create merge in the main

--- jest/index.js
+++ jest/index.js
@@ -1,4 +1,15 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+// Unit Test
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// module.exports = sum;
+
+//Unit Test with async function
+// const fetctUser = async () => {
+//   return { id: 1, name: "John Doe" };
+// };
+
+// console.log(fetctUser());
+
+// module.exports = fetctUser;
--- jest/sum.test.js
+++ jest/sum.test.js
@@ -1,5 +1,24 @@
-const sum = require("./index.js");
+// const fetchUser = require("./index.js");
 
-test("Must be sum 2 number", () => {
-  expect(sum(2, 3)).toBe(5);
-});
+//Unit test
+// test("Must be sum 2 number", () => {
+//   expect(sum(2, 3)).toBe(5);
+// });
+
+// test("Must be show user data ", async () => {
+//   const data = await fetchUser();
+//   expect(data).toEqual({ id: 1, name: "John Doe" });
+// });
+
+// const usuario = {
+//   saudacao: (nome) => `Olá, ${nome}!`,
+// };
+// console.log(usuario.saudacao("Mateus"));
+
+// jest.spyOn(usuario, "saudacao");
+
+// test("Espionando chamadas de usuario.saudacao", () => {
+//   usuario.saudacao("Mateus");
+
+//   expect(usuario.saudacao).toHaveBeenCalledWith("Mateus"); // ✅ Verifica se foi chamada
+// });
