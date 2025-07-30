609f13bbb671f241c67559a677d9d7ca9e294a5c
Matheus Fábio <matheusfabiors@gmail.com>
2025-07-30 00:05:50 -0300

    learning about the Array manipulation

--- iterator/iterador.js
+++ iterator/iterador.js
@@ -1,21 +1,34 @@
-const values = [1, 5, 4, 5, 2, 3]
+// const values = [1, 5, 4, 5, 2, 3]
 
-const it_values = values[Symbol.iterator]()
-console.log(it_values.next());
-console.log(it_values.next());
-console.log(it_values.next());
-console.log(it_values.next());
-console.log(it_values.next());
-console.log(it_values.next());
-console.log(it_values.next());
+// const it_values = values[Symbol.iterator]()
+// console.log(it_values.next());
+// console.log(it_values.next());
+// console.log(it_values.next());
+// console.log(it_values.next());
+// console.log(it_values.next());
+// console.log(it_values.next());
+// console.log(it_values.next());
 
 
-export default function iterator([value]) {
-  return value.next()
+// function iterator([value]) {
+//   return value.next()
+// }
+
+// iterator(values)
+
+// console.log(iterator);
+
+
+export default function Iterador(array) {
+  let index = 0;
+
+  return {
+    next: function () {
+      if (index < array.length) {
+        return { value: array[index++], done: false };
+      } else {
+        return { value: undefined, done: true };
+      }
+    }
+  };
 }
-
-iterator(values)
-
-console.log(iterator);
-
-
